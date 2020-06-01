c04504f969f64829f65ecfdb2fd9cbbeab05715b
Miari <maryna.boroday@gmail.com>
2020-06-01 21:39:22 +0200

    tempates moved to resources, methods for printing ov Sys/Env variablem were added

--- src/main/java/com/boroday/userstore/Starter.java
+++ src/main/java/com/boroday/userstore/Starter.java
@@ -7,16 +7,23 @@ import com.boroday.userstore.service.UserService;
 import com.boroday.userstore.service.impl.DefaultUserService;
 import com.boroday.userstore.util.PropertiesReader;
 import com.boroday.userstore.web.servlet.*;
+import lombok.extern.slf4j.Slf4j;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
 import javax.sql.DataSource;
+import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
 
+@Slf4j
 public class Starter {
     public static void main(String[] args) throws Exception {
+
+        printSystemVariables();
+        printEnvironmentVariables();
 
         PropertiesReader propertiesReader = new PropertiesReader("application.properties");
         Properties properties = propertiesReader.getProperties();
@@ -57,5 +64,25 @@ public class Starter {
         server.setHandler(context);
 
         server.start();
+    }
+
+    public static void printSystemVariables() {
+        log.info("System variables START");
+        Properties properties = System.getProperties();
+        Set<Map.Entry<Object, Object>> entries = properties.entrySet();
+        for (Map.Entry<Object, Object> entry : entries) {
+            log.info(entry.getKey() + " = " + entry.getValue());
+        }
+        log.info("System variables STOP");
+    }
+
+    public static void printEnvironmentVariables() {
+        log.info("Environment variables START");
+        Map <String, String> properties = System.getenv();
+        Set<Map.Entry<String, String>> entries = properties.entrySet();
+        for (Map.Entry<String, String> entry : entries) {
+            log.info(entry.getKey() + " = " + entry.getValue());
+        }
+        log.info("Environment variables STOP");
     }
 }
