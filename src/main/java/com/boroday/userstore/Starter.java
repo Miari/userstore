3671455155521b5a972bcefd29dbc67437fc8804
miari <maryna.boroday@gmail.com>
2020-08-23 15:29:31 +0200

    conflicts resolving

--- src/main/java/com/boroday/userstore/Starter.java
+++ src/main/java/com/boroday/userstore/Starter.java
@@ -1,92 +1,29 @@
-<<<<<<< HEAD
 package com.boroday.userstore;
 
-import com.boroday.ioc.context.ApplicationContext;
-import com.boroday.ioc.context.ClassPathApplicationContext;
-import com.boroday.userstore.dao.DataSourceFactory;
-import com.boroday.userstore.dao.UserDao;
-import com.boroday.userstore.dao.jdbc.JdbcUserDao;
-import com.boroday.userstore.service.UserService;
-import com.boroday.userstore.service.impl.DefaultUserService;
-import com.boroday.userstore.util.PropertiesReader;
-import com.boroday.userstore.web.servlet.*;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.DefaultServlet;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-
-import javax.sql.DataSource;
-import java.util.Properties;
-
-public class Starter {
-    public static void main(String[] args) throws Exception {
-
-        PropertiesReader propertiesReader = new PropertiesReader("application.properties");
-        Properties properties = propertiesReader.getProperties();
-        DataSource dataSource = DataSourceFactory.getDataSource(properties);
-
-        UserDao userDao = new JdbcUserDao(dataSource);
-        UserService userService = new DefaultUserService(userDao);
-
-        AllUsersServlet allUsersServlet = new AllUsersServlet(userService);
-        SignInServlet signInServlet = new SignInServlet(userService);
-        SignOutServlet signOutServlet = new SignOutServlet(userService);
-        AddNewUserServlet addNewUserServlet = new AddNewUserServlet(userService);
-        EditUserServlet editUserServlet = new EditUserServlet(userService);
-        SearchUserServlet searchUserServlet = new SearchUserServlet(userService);
-        RemoveUserServlet removeUserServlet = new RemoveUserServlet(userService);
-        DefaultServlet defaultServlet = new DefaultServlet();
-
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.addServlet(new ServletHolder(allUsersServlet), "/users");
-        context.addServlet(new ServletHolder(signInServlet), "/signin");
-        context.addServlet(new ServletHolder(signOutServlet), "/signout");
-        context.addServlet(new ServletHolder(addNewUserServlet), "/users/add");
-        context.addServlet(new ServletHolder(removeUserServlet), "/users/remove");
-        context.addServlet(new ServletHolder(editUserServlet), "/users/edit");
-        context.addServlet(new ServletHolder(searchUserServlet), "/users/search");
-
-        ServletHolder servletHolder = new ServletHolder("default", defaultServlet);
-        servletHolder.setInitParameter("resourceBase", "./src/main/resources/");
-        context.addServlet(servletHolder, "/");
-
-        //changed to DefaultServlet
-        //context.addServlet(new ServletHolder(new GetStaticResourcesServlet()), "/");
-
-        //PropertiesReader propertiesReader = ServiceLocator.getService(PropertiesReader.class);
-        int port = propertiesReader.getPropertyInt("server.port");
-
-        Server server = new Server(port);
-        server.setHandler(context);
-
-        server.start();
-    }
-}
-=======
-package com.boroday.userstore;
-
-import com.boroday.userstore.dao.DataSourceFactory;
-import com.boroday.userstore.dao.UserDao;
-import com.boroday.userstore.dao.jdbc.JdbcUserDao;
-import com.boroday.userstore.service.UserService;
-import com.boroday.userstore.service.impl.DefaultUserService;
-import com.boroday.userstore.util.PropertiesReader;
-import com.boroday.userstore.web.servlet.*;
 import lombok.extern.slf4j.Slf4j;
+import java.util.Map;
+import java.util.Set;
+
+//import com.boroday.ioc.context.ApplicationContext;
+//import com.boroday.ioc.context.ClassPathApplicationContext;
+import com.boroday.userstore.dao.DataSourceFactory;
+import com.boroday.userstore.dao.UserDao;
+import com.boroday.userstore.dao.jdbc.JdbcUserDao;
+import com.boroday.userstore.service.UserService;
+import com.boroday.userstore.service.impl.DefaultUserService;
+import com.boroday.userstore.util.PropertiesReader;
+import com.boroday.userstore.web.servlet.*;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
 import javax.sql.DataSource;
-import java.util.Map;
 import java.util.Properties;
-import java.util.Set;
 
 @Slf4j
 public class Starter {
     public static void main(String[] args) throws Exception {
-
         printSystemVariables();
         printEnvironmentVariables();
 
@@ -104,6 +41,8 @@ public class Starter {
         EditUserServlet editUserServlet = new EditUserServlet(userService);
         SearchUserServlet searchUserServlet = new SearchUserServlet(userService);
         RemoveUserServlet removeUserServlet = new RemoveUserServlet(userService);
+        DefaultServlet defaultServlet = new DefaultServlet();
+
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.addServlet(new ServletHolder(allUsersServlet), "/users");
@@ -114,7 +53,6 @@ public class Starter {
         context.addServlet(new ServletHolder(editUserServlet), "/users/edit");
         context.addServlet(new ServletHolder(searchUserServlet), "/users/search");
 
-        DefaultServlet defaultServlet = new DefaultServlet();
         ServletHolder servletHolder = new ServletHolder("default", defaultServlet);
         servletHolder.setInitParameter("resourceBase", "./src/main/resources/");
         context.addServlet(servletHolder, "/");
@@ -123,8 +61,6 @@ public class Starter {
         //context.addServlet(new ServletHolder(new GetStaticResourcesServlet()), "/");
 
         //PropertiesReader propertiesReader = ServiceLocator.getService(PropertiesReader.class);
-
-
         int port = propertiesReader.getPropertyInt("server.port");
 
         Server server = new Server(port);
@@ -146,7 +82,6 @@ public class Starter {
         log.info("SYSTEM VARIABLES STOP");
     }
 
-
     private static void printEnvironmentVariables() {
         log.info("ENVIRONMENT VARIABLES START");
         Map<String, String> properties = System.getenv();
@@ -158,4 +93,4 @@ public class Starter {
         log.info("ENVIRONMENT VARIABLES STOP");
     }
 }
->>>>>>> b600e31059ff5f0a1ecfa2e5739cb23e55da6ac6
+
