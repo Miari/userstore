8fefde05ac69a0d983aea180ff75101c8b7c02db
miari <maryna.boroday@gmail.com>
2020-10-09 23:42:19 +0200

    Assets servlet was added

---
+++ src/main/java/com/boroday/userstore/web/servlet/AssetsServlet.java
@@ -0,0 +1,42 @@
+package com.boroday.userstore.web.servlet;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Slf4j
+public class AssetsServlet extends HttpServlet {
+    private static final int BUFFER_SIZE = 8192;
+
+    @Override
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws IOException {
+        log.info("Resources are requested");
+
+        System.out.println("here");
+        String requestURI = request.getRequestURI().substring(1);
+
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(requestURI)) {
+            byte[] buffer = new byte[BUFFER_SIZE];
+            int numBytesRead;
+            while ((numBytesRead = inputStream.read(buffer)) > 0) {
+                response.getOutputStream().write(buffer, 0, numBytesRead);
+            }
+            response.setStatus(HttpServletResponse.SC_OK);
+
+        } catch (NullPointerException e) {
+            log.error("Resources URI is not found");
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            throw new RuntimeException("Not possible to get resource by the request: " + requestURI);
+        } catch (IOException e) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            throw new RuntimeException("Not possible to get resource by the request: " + requestURI, e);
+        }
+
+    }
+}
